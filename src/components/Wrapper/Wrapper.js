6fa42eb7d2aec738a172b3ab8c2ccbe0ea87a425
Vikas Gautam <priydarshivikas123@gmail.com>
2024-06-12 21:27:24 +0530

    remove token for login

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -1,19 +1,19 @@
-import React, {useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+// import React, {useContext, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
 import "./wrapper.css";
 import SideBar from "../SideBar/Index";
 import Header from "../Header/Header";
-import AuthContext from "../Contexts/AuthContexts";
+// import AuthContext from "../Contexts/AuthContexts";
 
 function Wrapper({ children }) {
-  const navigate = useNavigate(); 
-  const { userInfo } = useContext(AuthContext);
+  // const navigate = useNavigate(); 
+  // const { userInfo } = useContext(AuthContext);
 
-  useEffect(() => {
-    if (!userInfo.token) {
-      navigate("/login"); 
-    }
-  }, [userInfo, navigate]); 
+  // useEffect(() => {
+  //   if (!userInfo.token) {
+  //     navigate("/login"); 
+  //   }
+  // }, [userInfo, navigate]); 
 
   return (
     <>
